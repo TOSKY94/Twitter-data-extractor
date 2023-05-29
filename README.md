5bde651c01d62a32dac13e52882fb698e4652ef8
Patrick-Oguaju <32589922+TOSKY94@users.noreply.github.com>
2023-05-29 16:26:12 +0100

    created

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Twitter data extractor
- Twitter data extractor
+# Twitter Data Scraper
+
+This project is a Python script that utilizes the Tweepy library to scrape tweets from Twitter based on specific parameters, such as hashtags, date range, and optional location. The scraped tweets are saved to a CSV file for further analysis.
+
+## Prerequisites
+
+- Python 3.x
+- Tweepy library: Install it by running `pip install tweepy`
+
+## Getting Started
+
+
+1. Create a Twitter Developer account and obtain API credentials (consumer key, consumer secret, access token, and access token secret). Make sure you have read access to the Twitter API.
+
+2. Create a file named `credentials.txt` in the project directory and place your Twitter API credentials inside it in the format:
+consumer_key=YOUR_CONSUMER_KEY
+consumer_secret=YOUR_CONSUMER_SECRET
+access_token=YOUR_ACCESS_TOKEN
+access_token_secret=YOUR_ACCESS_TOKEN_SECRET
+
+
+3. Modify the script parameters in the `scrape_tweets` function within the `twitter_data_scraper.py` file. Update the hashtags, date range, and location as per your requirements.
+
+4. Run the script: python twitter_data_scraper.py
+
+
+5. The scraped tweets will be saved to a CSV file named `scraped_tweets.csv` in the project directory.
+
+## Customization
+
+- You can modify the script to perform additional processing on the scraped tweets or store them in a different format.
+- Feel free to adjust the search parameters, such as hashtags, date range, and location, based on your needs.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Disclaimer
+
+Please be aware of Twitter's terms of service and API usage policy. Ensure that your usage of the Twitter API is compliant with
+
+
+
---
+++ twitter_data_scraper.py
@@ -0,0 +1,67 @@
+import tweepy
+import csv
+import datetime
+
+# Load Twitter API credentials from a separate file
+def load_credentials(file_path):
+    credentials = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            key, value = line.strip().split('=')
+            credentials[key] = value
+    return credentials
+
+# Authenticate to Twitter
+def authenticate_twitter_api(credentials):
+    auth = tweepy.OAuthHandler(credentials['consumer_key'], credentials['consumer_secret'])
+    auth.set_access_token(credentials['access_token'], credentials['access_token_secret'])
+    return tweepy.API(auth)
+
+# Function to scrape tweets based on parameters
+def scrape_tweets(api, hashtags, start_date, end_date, location):
+    tweets = []
+    for hashtag in hashtags:
+        query = f'#{hashtag}'
+        if location:
+            query += f' near:"{location}"'
+
+        # Scrape tweets within the date range
+        for tweet in tweepy.Cursor(api.search, q=query, lang='en', tweet_mode='extended').items():
+            if tweet.created_at < start_date:
+                # Reached tweets before the start date, stop scraping
+                return tweets
+
+            if tweet.created_at <= end_date:
+                # Add tweet to the list
+                tweets.append(tweet._json)
+            else:
+                # Move on to the next hashtag
+                break
+
+    return tweets
+
+# Save tweets to a CSV file
+def save_tweets_to_csv(tweets, file_path):
+    keys = tweets[0].keys() if tweets else []
+    with open(file_path, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(tweets)
+
+# Example usage
+hashtags = ['example1', 'example2']  # List of hashtags to search for
+start_date = datetime.datetime(2023, 5, 1, 0, 0, 0)  # Start date of the date range
+end_date = datetime.datetime(2023, 5, 15, 0, 0, 0)  # End date of the date range
+location = 'New York City'  # Location to search near (optional)
+
+# Load API credentials from file
+credentials = load_credentials('credentials.txt')
+
+# Authenticate to Twitter API
+api = authenticate_twitter_api(credentials)
+
+# Scrape tweets
+scraped_tweets = scrape_tweets(api, hashtags, start_date, end_date, location)
+
+# Save tweets to a CSV file
+save_tweets_to_csv(scraped_tweets, 'scraped_tweets.csv')
